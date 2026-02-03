682af63c93765e3ce8e89669363983c4a24a3b8e
Mep <80524667+xavariusfelk@users.noreply.github.com>
2026-02-03 02:03:08 -0700

    lets not talk about it

---
+++ controllers/renderHome.js
@@ -0,0 +1,8 @@
+// I mean, there's not much to do xD
+function renderHome (req, res) {
+    res.render('index')
+}
+
+module.exports = {
+    renderHome
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { renderHome } = require('../controllers/renderHome');
 
-router.get('/', (req, res) => {
-    res.render('index')
-})
+router.get('/', renderHome)
 
 module.exports = router;
